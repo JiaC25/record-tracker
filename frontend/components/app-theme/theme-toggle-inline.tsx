e7868de0137d46ddc301e0b72bd578173aea2f25
JC <pjcpua1996@gmail.com>
2025-06-25 00:53:11 +0930

    Fix theme toggle animation and cleanup some unused tailwind classes

--- frontend/components/app-theme/theme-toggle-inline.tsx
+++ frontend/components/app-theme/theme-toggle-inline.tsx
@@ -27,20 +27,20 @@ const ThemeToggleInline = () => {
         <div className="flex items-center justify-center">
             {/* Custom Toggle Switch */}
             <button onClick={toggleTheme}
-                className="inline-flex bg-accent h-6 w-11 items-center rounded-full border duration-300 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-offset-background leading-4">
+                className="inline-flex bg-accent h-5 w-9 items-center rounded-full border hover:border-slate-500 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-offset-background leading-4">
                 {/* Toggle Circle */}
                 <span
                     className={cn(
-                        'inline-block h-5 w-5 transform rounded-full transition-all duration-300 ease-in-out bg-background',
-                        isDark ? 'translate-x-5 ml-0.5' : 'translate-x-0.5 ml-0',
+                        'inline-block bg-background h-4 w-4 rounded-full !transition-transform duration-300',
+                        isDark ? 'translate-x-4 ml-[1px]' : 'translate-x-0.5 -ml-[1px]',
                     )}
                 >
                     {/* Icon Container */}
                     <span className="flex h-full w-full items-center justify-center">
                         {isDark ? (
-                            <Moon className="!h-2.5 !w-2.5 transition-all duration-300" />
+                            <Moon className="!h-2.5 !w-2.5" />
                         ) : (
-                            <Sun className="!h-2.5 !w-2.5 transition-all duration-300" />
+                            <Sun className="!h-2.5 !w-2.5" />
                         )}
                     </span>
                 </span>
